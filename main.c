35d5420cdf7621eb64eec293b1c2899c382c6ab9
Alex Linares <alexlinares3006@gmail.com>
2026-05-17 18:50:07 -0500

    added subtraticion branch

--- main.c
+++ main.c
@@ -4,4 +4,11 @@
 int main() {
     int x, y;
     printf("Hello git\n");
+
+    printf("ins num 1: ");
+    scanf("%d", &x);
+    printf("ins num2: ");
+    scanf("%d", &y);
+
+    printf("%d - %d = %d", x, y, x - y);
 }
