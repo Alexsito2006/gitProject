189ce935d759a0bfb2877ec4461b3f57f05c76d2
Alex Linares <alexlinares3006@gmail.com>
2026-05-17 18:42:35 -0500

    added a simple addition area

--- main.c
+++ main.c
@@ -4,4 +4,11 @@
 int main() {
     int x, y;
     printf("Hello git\n");
+
+    printf("ins first num: ");
+    scanf("%d", &x);
+    printf("ins seccond num: ");
+    scanf("%d", &y);
+
+    printf("%d + %d = %d\n", x, y, x + y);
 }
